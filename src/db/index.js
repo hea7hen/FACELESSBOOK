37ee33962ae9e9c85eab8d46e06854097fae15d4
abhishek nair <abhisheknair@gmail.com>
2024-08-11 18:09:26 +0530

    feat: add new feature

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+
+dotenv.config(); // Ensure this is called to load .env variables
 
 /** @type {typeof mongoose | undefined} */
 export let dbInstance = undefined;
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
+    const dbURI = process.env.MONGODB_URI; // Ensure this is correctly set
+    console.log("MongoDB URI:", dbURI); // Log without backticks or semicolon
+    const connectionInstance = await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     dbInstance = connectionInstance;
     console.log(
       `\n☘️  MongoDB Connected! Db host: ${connectionInstance.connection.host}\n`
